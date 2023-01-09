b72c4c2d27ed53030548e0806dae8d0b31216bee
Júlio Alves <59064168+juliomiguelalves@users.noreply.github.com>
2023-01-09 23:21:29 +0000

    Revert "Revert "navdrawer fixed""
    This reverts commit ca7fad5c01c723ac0c823766cc08070c76caafb4.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <navDraw></navDraw>
         <appHeader></appHeader>
-        <h1 class="pa-md-4 mx-lg-auto text-center " > 👷 Serviço em Desenvolvimento </h1>
+        <navDraw></navDraw>
+        <h1 class="pa-md-4 mx-lg-auto text-center" style="margin-top:100px"> 👷 Serviço em Desenvolvimento </h1>
     </div>
 </template>
 
