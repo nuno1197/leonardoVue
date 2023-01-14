48dcc5ddd9b37dc08ef7d531bd247441ce108952
Júlio Alves <59064168+juliomiguelalves@users.noreply.github.com>
2023-01-14 16:12:13 +0000

    missing

--- src/components/header.vue
+++ src/components/header.vue
@@ -23,28 +23,25 @@
             <v-dialog @keydown.esc="about = false" v-model="about" scrollable width="500">
                 <v-card>
                     <v-toolbar v-bind:color="colorCode" dark>
-                        <h2>Saber mais</h2>
+                    <h2 class="white--text" :style="{ marginLeft: '15px' }">Saber Mais</h2>
                     </v-toolbar>
-                    <v-divider
-                    class="mx-4"
-                    horizontal
-                ></v-divider>
 
-                    <v-card-text class="change-font" style="white-space: pre-line"
-                    >Saber mais</v-card-text
-                    >
+                    <v-divider class="mx-4" horizontal></v-divider>
+
+                    <v-card-text class="grey--text change-font mt-6" style="white-space: pre-line">
+                    Texto saber mais
+                    </v-card-text>
                     <v-card-actions>
                     <v-spacer></v-spacer>
-                    
-                    <v-tooltip bottom> 
-                        <template v-slot:activator="{ on }">
-                            <v-btn depressed color="white" @click="about=false" v-on="{ on }">
-                            <v-icon large>mdi-exit-to-app</v-icon>
-                            </v-btn>
+
+                    <v-tooltip bottom>
+                        <template #activator="{ on }">
+                        <v-btn @click="dialog=false" v-bind="on">
+                            <v-icon size="30">mdi-exit-to-app</v-icon>
+                        </v-btn>
                         </template>
                         <span>Sair</span>
                     </v-tooltip>
-
                     </v-card-actions>
                 </v-card>
             </v-dialog>
