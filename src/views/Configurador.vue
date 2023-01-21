14cade493909dbfccc2efcec021288acfc14b9e3
Júlio Alves <59064168+juliomiguelalves@users.noreply.github.com>
2023-01-21 14:57:22 +0000

    some changes on color configurator

--- src/views/Configurador.vue
+++ src/views/Configurador.vue
@@ -2,40 +2,41 @@
     <div id="configurador">
         <appHeader :ajuda='ajuda'></appHeader>
         <navDraw></navDraw>
-        <v-card height="180%" width="180%">
-            <v-form ref="form" method="post" enctype="multipart/form-data">
+        <v-form ref="form" method="post" enctype="multipart/form-data">
+            <h1 class="title">Configurador de skin</h1>
             <v-container>
-                <v-row>
-                <v-col md="5">
-                    Cor dos Botões
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
-                <v-col md="5">
-                    Cor do Header
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
-                <v-col md="5">
-                    Cor do Footer
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
-                <v-col md="5">
-                    Cor da Navbar
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
-                <v-col md="5">
-                    Cor de Fundo
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
-                <v-col md="5">
-                    Cor da Letra
-                    <color-picker v-model="selectedColor"></color-picker>
-                </v-col>
+                <v-row align-content="center" fill-height>
+                    <v-col cols="6" md="5">
+                        Cor dos Botões
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
+                    <v-col cols="6" md="5">
+                        Cor do Header
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
+                    <v-col cols="6" md="5">
+                        Cor do Footer
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
+                    <v-col cols="6" md="5">
+                        Cor da Navbar
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
+                    <v-col cols="6" md="5">
+                        Cor de Fundo
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
+                    <v-col cols="6" md="5">
+                        Cor da Letra
+                        <color-picker v-model="selectedColor"></color-picker>
+                    </v-col>
                 </v-row>
             </v-container>
-            </v-form>
-        </v-card>
-    </div>
-</template>
+            <v-btn class="primary" @click="submitForm">Submit</v-btn>
+            <!-- <v-btn class="secondary" @click="resetForm">Reset</v-btn> -->
+        </v-form>
+        </div>
+  </template>
 
 <script>
 import Header from '../components/header.vue'
@@ -43,25 +44,36 @@ import NavDraw from '../components/navDraw.vue'
 import ColorPicker from '../components/colorPicker.vue'
 
 export default {
-    data() { return {}},
+    data() { 
+    return {
+        colorCode: this.$store.getters.skinColor
+    }
+    },
     components:{
             'appHeader': Header,
             'navDraw':NavDraw,
             'colorPicker': ColorPicker
+    }, 
+    methods: {
+    submitForm() {
     },
+    resetForm() {
+      this.selectedColor = ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-  #configurador *{
-            box-sizing: border-box;
-  }
-  #configurador{
-            margin: 200px auto;
-            max-width: 800px;
-            margin-bottom: 80px;
-  }
-  label {
+    #configurador *{
+        box-sizing: border-box;
+    }
+    #configurador{
+        margin: 200px auto;
+        max-width: 800px;
+        margin-bottom: 80px;
+    }
+    label {
         color: white;
         padding: 8px;
         font-family: Arial, sans-serif;
@@ -69,10 +81,10 @@ export default {
         font-size: 15px;
     }
     #bttAdd{
-        background-color: v-bind(colorCode);
+        background-color: "colorCode";
     }
 
     color-picker {
-        background-color: v-bind(colorCode);
+        background-color: "colorCode";
     }
 </style>
